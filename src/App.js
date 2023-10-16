7accec9ccf89d17a511b697adcfdada9c3a468dd
Avery Ramirez <68864812+averyramirez@users.noreply.github.com>
2023-10-15 17:23:04 -0700

    Tarot API
    Added api call for tarot cards, styled page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import React from "react";
+import axios from "axios";
 
 function App() {
+  const [cardName, setCardName] = useState("");
+  const [cardDesc, setCardDesc] = useState("");
+  const getCards = () => {
+    axios
+      .get("https://tarot-api-3hv5.onrender.com/api/v1/cards/random?n=1")
+      .then((res) => {
+        console.log(res.data.cards[0]);
+        setCardName(res.data.cards[0].name);
+        setCardDesc(res.data.cards[0].desc);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="tarot">
+        <div className="descBox">
+          <h1 className="title">Daily Tarot Card Reading</h1>
+          <button onClick={getCards} className="cardButton">
+            Click to see your fortune!
+          </button>
+          <h2 className="cardName">{cardName}</h2>
+          <p className="cardDesc">{cardDesc}</p>
+        </div>
+      </div>
     </div>
   );
 }
